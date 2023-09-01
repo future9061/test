bd590b7efed3e35710c80082403223d09aec5060
Lee Mi Rae <132829711+future9061@users.noreply.github.com>
2023-09-01 13:43:58 +0900

    Add files via upload

---
+++ client/components/MsgItem.js
@@ -0,0 +1,32 @@
+//text update
+
+import MsgInput from "./MsgInput"
+
+const MsgItem = ({ id, userId, timestamp, text, onUpdate, onDelete, isEditing, startEdit }) => (
+  <li className="message_item">
+    <h3>{userId}
+      <sub>
+        {new Date(timestamp).toLocaleString('ko-KR', {
+          year: 'numeric',
+          month: 'numeric',
+          day: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: true,
+        })}
+      </sub>
+    </h3>
+    {isEditing ? (
+      <>
+        <MsgInput mutate={onUpdate} text={text} id={id} />
+      </>
+    ) : text}
+
+    <div className="message_buttons">
+      <button onClick={startEdit}>수정</button>
+      <button onClick={onDelete}>삭제</button>
+    </div>
+  </li >
+)
+
+export default MsgItem
